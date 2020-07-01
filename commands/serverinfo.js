96f4d8e17b5710ecc5e6cb1e65765b3481f2c218
Lyon Jenkins <lyonjenkins@gmail.com>
2020-07-01 12:56:26 -0500

    Used queryServer function

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,6 +1,4 @@
-const Gamedig = require('gamedig');
-const config = require('../config.json');
-import { newServerInfoEmbed } from '../functions/helperFuncs';
+import { queryServer, newServerInfoEmbed } from '../functions/helperFuncs';
 
 export const name = 'serverinfo',
 	description = 'Returns info on the public server',
@@ -11,19 +9,11 @@ export const name = 'serverinfo',
 	disabled = false;
 
 export function execute(message, args) {
-	const defaultServer = config.servers.find(x => x.name === config.defaultServer);
-	Gamedig.query({
-		type: 'squad',
-		host: defaultServer.ip,
-		port: parseInt(defaultServer.queryPort),
-		maxAttempts: 5,
-	}).then((state) => {
-		const serverEmbed = newServerInfoEmbed(state);
-		message.channel.send(serverEmbed).then(msg => {
+	queryServer().then(response => {
+		message.channel.send(newServerInfoEmbed(response)).then(msg => {
 			msg.react('🔄');
 		});
-	}).catch((error) => {
-		console.log(error);
-		console.log("Server is offline");
+	}).catch(error => {
+		if(error) console.log(error);
 	});
 }
