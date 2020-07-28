1ff5056ada835989ac9ab15e5a54953eb42db6df
Lyon Jenkins <lyonjenkins@gmail.com>
2020-07-28 11:29:58 -0500

    Added reaction role adder/remover

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 import checkForRefreshReaction from './checkForRefreshReaction';
 import properArgs from './properArgs';
+import reactionGiveRole from './reactionGiveRole';
 
 export {
 	checkForRefreshReaction,
-	properArgs
+	properArgs,
+	reactionGiveRole
 }
---
+++ functions/reactionGiveRole.js
@@ -0,0 +1,15 @@
+import { allowableAddRoles } from '../config';
+
+export default function reactionGiveRole(message, reaction, user) {
+	for (const roleId of allowableAddRoles) {
+		if(message.content.indexOf(roleId) !== -1) {
+			 message.guild.members.fetch(user.id).then(GuildMember => {
+			 	if(!GuildMember.roles.cache.find(role => role.id === roleId)) {
+			 		GuildMember.roles.add(roleId);
+				} else {
+					GuildMember.roles.remove(roleId);
+				}
+			 });
+		}
+	}
+}
