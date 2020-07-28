6aca2f3e0c0ac2a19b921e4597bd0e1f876bdb41
Lyon Jenkins <lyonjenkins@gmail.com>
2020-07-28 11:29:47 -0500

    Added arma test command

---
+++ commands/arma.js
@@ -0,0 +1,27 @@
+const Gamedig = require('gamedig');
+import { servers } from '../config';
+
+export default {
+	name: 'arma',
+	description:'Returns Arma server info',
+	server: {},
+	args: true,
+	guildOnly: true,
+	aliases: [''],
+	disabled: false,
+	execute(message, args, server) {
+		server = servers.find(x => x.name === args[0].toLowerCase());
+		if(!server) return message.reply('server not found.');
+
+		Gamedig.query({
+			type: 'arma3',
+			host: server.ip,
+			port: server.port,
+			maxAttempts: 10,
+		}).then(r => {
+			console.log(r);
+		}).catch(error => {
+			console.log(error);
+		});
+	}
+}
