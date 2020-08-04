44720826528b370c77c46cfd338dcbb068d87a1d
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-03 21:40:36 -0500

    Changed how roleIDs were given, specified by messageID and not some stupid way

--- src/functions/reactionGiveRole.js
+++ src/functions/reactionGiveRole.js
@@ -1,19 +1,21 @@
-import { allowableAddRoles } from '../../config';
+import { addRoleMessageIDs } from '../../config';
 import { log } from './';
 
 export default function reactionGiveRole(message, reaction, user, remove) {
 	log('Entered reactiveGiveRole function');
-	for (const roleId of allowableAddRoles) {
-		if(message.content.indexOf(roleId) !== -1) {
-			 message.guild.members.fetch(user.id).then(GuildMember => {
-			 	if(!GuildMember.roles.cache.find(role => role.id === roleId) && !remove) {
-			 		log(`Added roleID ${roleId} to user ${user.username}`);
-			 		GuildMember.roles.add(roleId);
+	for (const messageID of addRoleMessageIDs) {
+		if(message.id === messageID) {
+			const parsed = message.content.match(/([0-9])+/);
+			const roleId = parsed[0];
+			message.guild.members.fetch(user.id).then(GuildMember => {
+				if(!GuildMember.roles.cache.find(role => role.id === roleId) && !remove) {
+					log(`Added roleID ${roleId} to user ${user.username}`);
+					GuildMember.roles.add(roleId);
 				} else if(GuildMember.roles.cache.find(role => role.id === roleId) && remove) {
 					log(`Removed roleID ${roleId} from user ${user.username}`);
 					GuildMember.roles.remove(roleId);
 				}
-			 });
+			});
 		}
 	}
 }
