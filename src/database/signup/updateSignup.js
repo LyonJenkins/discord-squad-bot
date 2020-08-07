7982ff7b778c40ba721f86a8f2d8be5c1c23da1b
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-07 11:00:35 -0500

    Removed uncessary logging

--- src/database/signup/updateSignup.js
+++ src/database/signup/updateSignup.js
@@ -4,10 +4,8 @@ import { log } from '../../functions';
 
 export default function updateSignup(id, data) {
 	mongoose.connect('mongodb://localhost:27017/blueberrydb', {useNewUrlParser: true, useUnifiedTopology: true});
-	console.log(data);
 	Signup.findByIdAndUpdate(id, data, (err, signup) => {
 		if(err) console.log(err);
-		console.log(signup);
 		log('Updated signup');
 	});
 }
