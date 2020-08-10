1298b863a52432e1f257931904fbbac81efe97e3
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-09 19:53:10 -0500

    Added embedmsgid

--- src/database/models/signup.js
+++ src/database/models/signup.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const Signup = new mongoose.Schema({
 	name: String,
-	discordMessageID: String
+	discordMessageID: String,
+	discordSignupEmbedID: String
 });
 
 export default mongoose.model('Signup', Signup);
