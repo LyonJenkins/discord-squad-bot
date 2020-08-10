6276b34d2544041be0b399ac39a73a5873f44a5b
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-09 19:55:25 -0500

    Changed prramaters to take an object

--- src/database/signup/fetchSignups.js
+++ src/database/signup/fetchSignups.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 import { Signup } from '../models';
 
-export default async function fetchSignups() {
+export default async function fetchSignups(key) {
 	mongoose.connect('mongodb://localhost:27017/blueberrydb', {useNewUrlParser: true, useUnifiedTopology: true});
-	return await Signup.find({}, (err, signups) => {
+	if(!key) key = {};
+	return await Signup.find(key, (err, signups) => {
 		if (err) return console.log(err);
 		return signups;
 	});
