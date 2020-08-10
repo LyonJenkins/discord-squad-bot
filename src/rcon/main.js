405afe45050231dba775d719c360b608875048d8
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-09 19:52:27 -0500

    Updated rcon to end connection after command was sent

--- src/rcon/main.js
+++ src/rcon/main.js
@@ -5,46 +5,46 @@ export async function listPlayers(server) {
     if(!server) server = 'rcon test';
     const serverInfo = config.servers.find(x => x.name === server);
     const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.port, password: serverInfo.password
+        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
     });
-    rcon.send('ListPlayers').then(response => {
-        console.log(response);
+    return await rcon.send('ListPlayers').then(response => {
+        rcon.end();
         return response;
     });
 }
 
 export async function kickById(playerID, reason, server) {
-    if(!server) server = config.defaultServer;
+    if(!server) server = 'rcon test';
     const serverInfo = config.servers.find(x => x.name === server);
     const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.port, password: serverInfo.password
+        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
     });
-    rcon.send(`AdminKickById ${playerID} ${reason}`).then(response => {
-        console.log(response);
+    return await rcon.send(`AdminKickById ${playerID} ${reason}`).then(response => {
+        rcon.end();
         return response;
     });
 }
 
 export async function banByID(playerID, reason, length, server) {
-    if(!server) server = config.defaultServer;
+    if(!server) server = 'rcon test';
     const serverInfo = config.servers.find(x => x.name === server);
     const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.port, password: serverInfo.password
+        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
     });
-    rcon.send(`AdminBanById ${playerID} ${length} ${reason}`).then(response => {
-        console.log(response);
+    return await rcon.send(`AdminBanById ${playerID} ${length} ${reason}`).then(response => {
+        rcon.end();
         return response;
     });
 }
 
 export async function broadcast(message, server) {
-    if(!server) server = config.defaultServer;
+    if(!server) server = 'rcon test';
     const serverInfo = config.servers.find(x => x.name === server);
     const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.port, password: serverInfo.password
+        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
     });
-    rcon.send(`AdminBroadcast ${message}`).then(response => {
-        console.log(response);
+    return await rcon.send(`AdminBroadcast ${message}`).then(response => {
+        rcon.end();
         return response;
     });
 }
