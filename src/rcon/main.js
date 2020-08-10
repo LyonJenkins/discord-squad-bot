45ed26a872b4c912f0d63b643ecc1befc18227a7
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-10 15:51:10 -0500

    Updated how the rcon client is handled
    This will prevent timeouts by creating a single connection.

--- src/rcon/main.js
+++ src/rcon/main.js
@@ -1,50 +1,22 @@
 const { Rcon } = require("rcon-client");
 const config = require('../../config.json');
 
-export async function listPlayers(server) {
-    if(!server) server = 'rcon test';
-    const serverInfo = config.servers.find(x => x.name === server);
-    const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
-    });
-    return await rcon.send('ListPlayers').then(response => {
-        rcon.end();
-        return response;
-    });
-}
+export default class RconConnection {
+    constructor(server) {
+        this.server = server;
+    }
 
-export async function kickById(playerID, reason, server) {
-    if(!server) server = 'rcon test';
-    const serverInfo = config.servers.find(x => x.name === server);
-    const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
-    });
-    return await rcon.send(`AdminKickById ${playerID} ${reason}`).then(response => {
-        rcon.end();
-        return response;
-    });
-}
+    async main() {
+        const serverInfo = config.servers.find(x => x.name === this.server);
+        this.rcon = await Rcon.connect({
+            host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
+        });
 
-export async function banByID(playerID, reason, length, server) {
-    if(!server) server = 'rcon test';
-    const serverInfo = config.servers.find(x => x.name === server);
-    const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
-    });
-    return await rcon.send(`AdminBanById ${playerID} ${length} ${reason}`).then(response => {
-        rcon.end();
-        return response;
-    });
-}
+    }
 
-export async function broadcast(message, server) {
-    if(!server) server = 'rcon test';
-    const serverInfo = config.servers.find(x => x.name === server);
-    const rcon = await Rcon.connect({
-        host: serverInfo.ip, port: serverInfo.rconPort, password: serverInfo.password
-    });
-    return await rcon.send(`AdminBroadcast ${message}`).then(response => {
-        rcon.end();
-        return response;
-    });
+    async listPlayers() {
+        return await this.rcon.send('ListPlayers').then(response => {
+            return response;
+        });
+    }
 }
