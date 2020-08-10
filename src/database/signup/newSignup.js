dd9d1125386ecf5d28ed1e9d818624a9da5ba4e9
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-09 19:55:36 -0500

    Added a signup embed id

--- src/database/signup/newSignup.js
+++ src/database/signup/newSignup.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 import { Signup } from '../models';
 import { log } from '../../functions';
 
-export default function newSignup(discordMessage) {
+export default function newSignup(discordMessage, embedID) {
 	mongoose.connect('mongodb://localhost:27017/blueberrydb', {useNewUrlParser: true, useUnifiedTopology: true});
 	const newSignup = new Signup({
 		name: discordMessage.content,
 		discordMessageID: discordMessage.id,
-		reactionLog: []
+		discordSignupEmbedID: embedID,
 	});
 	newSignup.save().then(() => {
 		log('New signup created');
