322465a0a6b36de149e39267f318b9985ff92ddb
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-10 12:41:30 -0500

    Added embed and client uptime

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,4 +1,6 @@
 import { log } from '../functions';
+const Discord = require('discord.js');
+
 
 export default {
 	name: 'ping',
@@ -11,8 +13,29 @@ export default {
 	execute(message, args) {
 		log(`Entered ${this.name} command file`);
 		message.reply('Pinging').then(msg => {
-			msg.edit(`Ping is ${msg.createdAt - message.createdAt}ms`);
+			const pingEmbed = new Discord.MessageEmbed()
+				.setColor('#0099ff')
+				.setTitle(message.client.user.username)
+				.addFields(
+					{ name: 'Ping', value: `${msg.createdAt - message.createdAt}ms` },
+					{ name: 'Client Uptime', value: `${msToTime(message.client.uptime)}`},
+				)
+				.setTimestamp();
+			message.channel.send(pingEmbed)
+			msg.delete();
 		});
 	}
 }
 
+function msToTime(duration) {
+	let milliseconds = parseInt((duration % 1000) / 100),
+		seconds = Math.floor((duration / 1000) % 60),
+		minutes = Math.floor((duration / (1000 * 60)) % 60),
+		hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+	hours = (hours < 10) ? "0" + hours : hours;
+	minutes = (minutes < 10) ? "0" + minutes : minutes;
+	seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+	return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
