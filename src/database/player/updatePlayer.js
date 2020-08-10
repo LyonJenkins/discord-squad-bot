d56463a73923eac1a4003439f3eee06eb2bce2c3
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-09 19:55:01 -0500

    Removed logging

--- src/database/player/updatePlayer.js
+++ src/database/player/updatePlayer.js
@@ -6,7 +6,6 @@ export default function updatePlayer(id, data) {
 	mongoose.connect('mongodb://localhost:27017/blueberrydb', {useNewUrlParser: true, useUnifiedTopology: true});
 	Player.findByIdAndUpdate(id, data, (err, player) => {
 		if(err) console.log(err);
-		console.log(player);
 		log('Updated player');
 	});
 }
