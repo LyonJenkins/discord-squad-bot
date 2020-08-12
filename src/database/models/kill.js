cf078938c9e89f851a728a02001f996e9bcbad47
Lyon Jenkins <lyonjenkins@gmail.com>
2020-08-11 19:45:18 -0500

    Added additional data for each kill/wound

--- src/database/models/kill.js
+++ src/database/models/kill.js
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 const Kill = new mongoose.Schema({
 	killer: String,
 	victim: String,
+	role: String,
 	weapon: String,
+	map: String,
+	players: String,
+	server: String,
 	teamkill: Boolean,
 	createdTimestamp: Date,
-	serverName: String
+	serverName: String,
+	wound: Boolean
 });
 
 export default mongoose.model('Kill', Kill);
