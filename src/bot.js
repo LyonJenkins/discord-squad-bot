01fb5d8184cbf34bd55bda1f8d956b1435a058c5
Lyon Jenkins <lyonjenkins@gmail.com>
2020-09-01 14:05:57 -0500

    Added Guild_Presences intent

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER'] });
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'GUILD_PRESENCES'] });
 client.commands = new Discord.Collection();
 import * as commands from './commands';
 import { Server } from './server';
