b39f1c89464ed634785dda0d2887eef18e95a729
Lyon Jenkins <lyonjenkins@gmail.com>
2020-12-14 22:31:38 -0600

    Outputs string of role messages ids

--- src/commands/addRolesMessage.js
+++ src/commands/addRolesMessage.js
@@ -12,12 +12,15 @@ export default {
 	execute: async (message, args) => {
 		log(`Entered addRolesMessage command file`);
 		await message.channel.send('**Add Roles**\n' + 'To add yourself to the following roles, react with any emoji. To remove yourself from any of the roles, unreact with any emoji.');
+		const msgIds = [];
 		for(const role of allowableAddRoles) {
 			const msg = await message.channel.send('**');
+			msgIds.push(msg.id);
 			const discordRole = await message.guild.roles.fetch(role);
 			msg.edit(discordRole.toString());
 			msg.react('⬆️');
 		}
+		console.log(msgIds);
 		message.delete();
 	}
 }
